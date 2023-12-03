442f0dd9926ecab7f9ac6c9763be947d466fdc64
Kirill Smetanin <skrill3901@gmail.com>
2023-12-03 22:45:37 +0300

    feat: locale

--- service-frontend/src/app/index.tsx
+++ service-frontend/src/app/index.tsx
@@ -1,18 +1,27 @@
 import { FC } from 'react';
 import { observer } from 'mobx-react-lite';
+import { Outlet } from 'react-router-dom';
+
 import './app.css';
 
-import { Outlet } from 'react-router-dom';
 import { ConfigProvider, Layout } from 'antd';
+import ruRU from 'antd/locale/ru_RU';
+import enUS from 'antd/locale/en_US';
 
 import { darkTheme, lightTheme } from 'shared/theme';
 import { Header } from 'wigets';
 import { useStore } from './store';
 
+const locales = { ruRU, enUS };
+
 export const App: FC = () => {
   const { settings } = useStore();
+
   return (
-    <ConfigProvider theme={settings.theme === 'dark' ? darkTheme : lightTheme}>
+    <ConfigProvider
+      theme={settings.theme === 'dark' ? darkTheme : lightTheme}
+      locale={locales[settings.locale]}
+    >
       <Layout className="layout" style={{ minHeight: '100vh' }}>
         <Header />
         <Layout.Content className="container">
--- service-frontend/src/app/store/settings/index.ts
+++ service-frontend/src/app/store/settings/index.ts
@@ -1,10 +1,13 @@
 import { makeAutoObservable } from 'mobx';
 
 type ThemeMode = 'dark' | 'light';
+type Locale = 'ruRU' | 'enUS';
 
 const themeFromLocalStorage: ThemeMode | null = localStorage.getItem('theme') as ThemeMode;
+const localeFromLocalStorage: Locale | null = localStorage.getItem('locale') as Locale;
 export class Settings {
   theme: ThemeMode = themeFromLocalStorage || 'light';
+  locale: Locale = localeFromLocalStorage || 'ruRU';
 
   constructor() {
     makeAutoObservable(this);
@@ -13,5 +16,10 @@ export class Settings {
   setTheme(value: ThemeMode) {
     this.theme = value;
     localStorage.setItem('theme', value);
+  }
+
+  setLocale(value: Locale) {
+    this.locale = value;
+    localStorage.setItem('locale', value);
   }
 }
