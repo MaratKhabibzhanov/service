6b655171c24a6cfef23ee47fe2d55a7c62d78b2b
Kirill Smetanin <skrill3901@gmail.com>
2023-12-03 18:03:33 +0300

    config!: feat mobx

--- service-frontend/src/app/store/auth/index.ts
+++ service-frontend/src/app/store/auth/index.ts
@@ -1,5 +1,11 @@
+import { makeAutoObservable } from 'mobx';
+
 export class Auth {
   isAuth = false;
+
+  constructor() {
+    makeAutoObservable(this);
+  }
 
   setIsAuth(value: boolean) {
     this.isAuth = value;
