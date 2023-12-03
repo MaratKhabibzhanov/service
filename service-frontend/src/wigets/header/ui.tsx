220d065662a727c6bdb902f4b183442acb8ebb05
Kirill Smetanin <skrill3901@gmail.com>
2023-12-03 17:39:55 +0300

    fix: menu header

--- service-frontend/src/wigets/header/ui.tsx
+++ service-frontend/src/wigets/header/ui.tsx
@@ -1,10 +1,12 @@
 import { Dispatch, FC, SetStateAction } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 
+import { useStore } from 'app/store';
+import { AuthModal } from 'features';
+
 import { tton, Layout, Menu, Space, Switch, Typography } from 'antd';
 import { CheckOutlined, CloseOutlined } from '@ant-design/icons';
 import { menuItems } from './consts';
-import { AuthModal } from 'features';
 
 type HeaderProps = {
   isDarkTheme: boolean;
@@ -15,6 +17,8 @@ export const Header: FC<HeaderProps> = ({ isDarkTheme, setIsDarkTheme }) => {
   const { pathname } = useLocation();
   const navigate = useNavigate();
 
+  const { auth } = useStore();
+
   const handleChangeThemeMode = (value: boolean) => {
     if (value) localStorage.setItem('theme', 'dark');
     else localStorage.setItem('theme', 'light');
@@ -24,24 +28,28 @@ export const Header: FC<HeaderProps> = ({ isDarkTheme, setIsDarkTheme }) => {
   return (
     <Layout.Header className="header">
       <Typography.Text style={{ color: '#fff', fontSize: '20px' }}>Habib Service</Typography.Text>
-      <Menu
-        items={menuItems}
-        selectedKeys={[pathname]}
-        onClick={(e) => navigate(e.key)}
-        mode="horizontal"
-        className="menu"
-      />
-      <Space>
-        <Typography.Text style={{ color: '#fff' }}>Dark mode:</Typography.Text>
-        <Switch
-          checkedChildren={<CheckOutlined />}
-          unCheckedChildren={<CloseOutlined />}
-          checked={isDarkTheme}
-          onChange={handleChangeThemeMode}
-          style={{ marginBottom: '3px' }}
+      {auth.isAuth && (
+        <Menu
+          items={menuItems}
+          selectedKeys={[pathname]}
+          onClick={(e) => navigate(e.key)}
+          mode="horizontal"
+          className="menu"
         />
+      )}
+      <Space size="large">
+        <Space>
+          <Typography.Text style={{ color: '#fff' }}>Dark mode:</Typography.Text>
+          <Switch
+            checkedChildren={<CheckOutlined />}
+            unCheckedChildren={<CloseOutlined />}
+            checked={isDarkTheme}
+            onChange={handleChangeThemeMode}
+            style={{ marginBottom: '3px' }}
+          />
+        </Space>
+        <AuthModal />
       </Space>
-      <AuthModal />
     </Layout.Header>
   );
 };
