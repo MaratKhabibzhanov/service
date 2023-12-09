000113918af3cf85c3e1c9b2422dab28b1d08742
Kirill Smetanin <skrill3901@gmail.com>
2023-12-09 17:30:43 +0300

    feat: profiles api

--- service-frontend/src/shared/api/user-service.ts
+++ service-frontend/src/shared/api/user-service.ts
@@ -5,4 +5,21 @@ export default class UserService {
     const role: User = await $api.get('auth/users/me/').json();
     return role;
   }
+
+  static async updateProfile(body: User) {
+    const request: User = await $api.put('auth/users/me/', { json: body }).json();
+    return request;
+  }
+
+  static async changePassword(current_password: string, new_password: string) {
+    const body = { current_password, new_password };
+    const request = await $api.post('auth/users/set_password/', { json: body }).json();
+
+    return request;
+  }
+
+  static async removeProfile() {
+    const request = await $api.delete('auth/users/me/').json();
+    return request;
+  }
 }
