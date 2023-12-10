3cd898d5e69538e018bd37196f02f0d9a2ee0690
ascren <marat.khabibzhanov@mail.ru>
2023-12-10 23:07:37 +0500

    fix: staticfiles

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,5 @@
 
 sleep 10
 python manage.py migrate
+python manage.py collectstatic  --noinput
 gunicorn config.wsgi --bind 0.0.0.0:8000
--- web_service/config/settings.py
+++ web_service/config/settings.py
@@ -191,9 +191,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/api/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-MEDIA_URL = '/media/'
+MEDIA_URL = '/api/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
