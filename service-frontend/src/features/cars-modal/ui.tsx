f922141f3158f44be76228c44bbde4825523384a
Kirill Smetanin <skrill3901@gmail.com>
2023-12-10 23:57:14 +0300

    feat: models modal

--- service-frontend/src/features/cars-modal/ui.tsx
+++ service-frontend/src/features/cars-modal/ui.tsx
@@ -1,26 +1,52 @@
 import { FC, useEffect, useState } from 'react';
 
-import { Button, Modal } from 'antd';
+import { Button, Card, Flex, Image, Modal } from 'antd';
 import { AdditionalService } from 'shared/api';
 
 type CarsModalProps = {
-  currentCar?: number;
+  currentModel?: number;
+  setModel: (car: CarModel) => void;
 };
 
-const CarsModal: FC<CarsModalProps> = ({ currentCar }) => {
+const CarsModal: FC<CarsModalProps> = ({ currentModel, setModel }) => {
   const [open, setOpen] = useState(false);
   const [cars, setCars] = useState<CarModel[]>([]);
 
   useEffect(() => {
-    AdditionalService.getCarModels().then((cars) => {
-      setCars(cars.results);
-    });
-  }, []);
+    if (open) {
+      AdditionalService.getCarModels().then((carsData) => {
+        setCars(carsData.results);
+      });
+    }
+  }, [open]);
 
   return (
     <>
-      <Button onClick={() => setOpen(true)}>{currentCar ? 'Change' : 'Add'}</Button>
-      <Modal title="Select your car mode:" open={open} onCancel={() => setOpen(false)}></Modal>
+      <Button onClick={() => setOpen(true)}>{currentModel ? 'Change' : 'Add'}</Button>
+      <Modal title="Select your car mode:" open={open} onCancel={() => setOpen(false)} width={1028}>
+        <Flex gap={40}>
+          {cars.map((item) => (
+            <Card
+              hoverable
+              key={item.id}
+              cover={
+                <Image
+                  height={280}
+                  width={300}
+                  src={item.image}
+                  alt={item.model}
+                  style={{ objectFit: 'cover' }}
+                />
+              }
+            >
+              <Card.Meta
+                title={<Button onClick={() => setModel(item)}>{item.model}</Button>}
+                description={`Engine type: ${item.engine}`}
+              />
+            </Card>
+          ))}
+        </Flex>
+      </Modal>
     </>
   );
 };
