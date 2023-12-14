3efa1639e45d7120a128c910d417678651992175
ascren <marat.khabibzhanov@mail.ru>
2023-12-14 10:24:19 +0500

    fix: X-forvarded-proto

--- web_service/config/settings.py
+++ web_service/config/settings.py
@@ -58,6 +58,9 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+USE_X_FORWARDED_HOST = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
 CORS_ORIGIN_ALLOW_ALL = True
 
 CORS_ALLOW_HEADERS = [
