d8f0742355d0dab715a5110560b653324eaf28b8
Kirill Smetanin <skrill3901@gmail.com>
2024-01-21 21:55:23 +0300

    feat: disabled times range

--- service-frontend/src/shared/helpers/index.ts
+++ service-frontend/src/shared/helpers/index.ts
@@ -2,3 +2,4 @@ export { default as compose } from './compose';
 export { default as getRefreshTokens } from './get-refresh-tokens';
 export { default as getMenuItems } from './get-menu-items';
 export { default as getFullName } from './get-full-name';
+export { default as range } from './range';
--- service-frontend/src/widgets/registration-for-repairs-form/ui.tsx
+++ service-frontend/src/widgets/registration-for-repairs-form/ui.tsx
@@ -1,14 +1,16 @@
 import { FC, useLayoutEffect, useMemo, useState } from 'react';
 
-import { dayMilliseconds, formItemLayout } from 'shared/consts';
+import { formItemLayout } from 'shared/consts';
 import { RepairService } from 'shared/api';
-import { getFullName } from 'shared/helpers';
+import { getFullName, range } from 'shared/helpers';
 
 import { DatePicker, Form, Select } from 'antd';
+import { Dayjs } from 'dayjs';
 
 type FieldsType = {
   acceptor: Acceptor;
   day: string;
+  time: string;
 };
 
 export const RegistrationForRepairsForm: FC = () => {
@@ -22,7 +24,11 @@ export const RegistrationForRepairsForm: FC = () => {
     });
   }, []);
 
-  const sendForm = (values) => {
+  const changeDate = (date: Dayjs | null) => {
+    console.log(date?.format('YYYY-MM-DD'));
+  };
+
+  const sendForm = (values: FieldsType) => {
     console.log(values);
   };
 
@@ -36,6 +42,16 @@ export const RegistrationForRepairsForm: FC = () => {
     currentDate.setTime(currentDate.getTime());
     return currentDate;
   }, []);
+
+  const disabledTimes = () => {
+    const hours = range(0, 24);
+    const currentDisabledHours = [...hours.slice(0, 9), ...hours.slice(20, 24)];
+
+    return {
+      disabledHours: () => currentDisabledHours,
+      disabledMinutes: () => range(30, 60),
+    };
+  };
 
   return (
     <Form
@@ -58,7 +74,22 @@ export const RegistrationForRepairsForm: FC = () => {
         label="Date"
         rules={[{ required: true, message: 'Please input date!' }]}
       >
-        <DatePicker disabledDate={(d) => !d || d.isBefore(disableDates)} />
+        <DatePicker
+          disabledDate={(d) => !d || d.isBefore(disableDates)}
+          onChange={changeDate}
+          disabled={!form.getFieldValue('acceptor')}
+        />
+      </Form.Item>
+      <Form.Item
+        name="time"
+        label="Time"
+        rules={[{ required: true, message: 'Please input time!' }]}
+      >
+        <DatePicker.TimePicker
+          disabledTime={disabledTimes}
+          showSecond={false}
+          disabled={!form.getFieldValue('day')}
+        />
       </Form.Item>
     </Form>
   );
