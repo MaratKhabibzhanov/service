1e552bf514140bd7bb3b1af9a5836dc40fcd78d7
ascren <marat.khabibzhanov@mail.ru>
2024-01-28 22:01:35 +0500

    fix start bot on startapp

--- entrypoint.dev.sh
+++ entrypoint.dev.sh
@@ -2,5 +2,5 @@
 
 python manage.py migrate
 python manage.py collectstatic  --noinput
-python manage.py runserver 0.0.0.0:8000
 python manage.py telegram_bot
+python manage.py runserver 0.0.0.0:8000
