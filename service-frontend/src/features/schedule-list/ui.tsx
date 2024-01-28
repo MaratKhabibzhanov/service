c8508528cedeb8e83943b535378cc736958403af
Kirill Smetanin <skrill3901@gmail.com>
2024-01-28 19:57:25 +0300

    feat: ScheduleItem from request

--- service-frontend/src/features/schedule-list/ui.tsx
+++ service-frontend/src/features/schedule-list/ui.tsx
@@ -1,15 +1,28 @@
 import { ScheduleItem } from 'entities';
 import { FC } from 'react';
 import { getFullName } from 'shared/helpers';
+import { TIMES } from './consts';
 
 type ScheduleProps = {
   items: RegistrationForRepairs[];
 };
 
 const ScheduleList: FC<ScheduleProps> = ({ items }) => {
-  return items.map((item) => (
-    <ScheduleItem user={getFullName(item.acceptor)} car={item.car.car_model.model} key={item.id} />
-  ));
+  return TIMES.map((time) => {
+    const currentItem = items.find((item) => item.time.slice(0, 5) === time);
+
+    if (currentItem) {
+      return (
+        <ScheduleItem
+          user={getFullName(currentItem.acceptor)}
+          car={currentItem.car.car_model.model}
+          key={currentItem.id}
+        />
+      );
+    }
+
+    return undefined;
+  });
 };
 
 export default ScheduleList;
